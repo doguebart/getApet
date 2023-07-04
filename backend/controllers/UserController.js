c4d98fa0a19a2e932bd0eb853914d64e6abd75ef
doguebart <douglasweelber.2004@outlook.com>
2023-07-04 17:31:14 -0300

    creating function to get user token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,10 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Helpers
 const createUserToken = require("../helpers/create-users-token");
+const getToken = require("../helpers/get-token");
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -131,5 +135,21 @@ module.exports = class UserController {
     }
 
     await createUserToken(user, req, res);
+  }
+
+  static async checkUser(req, res) {
+    let currentUser;
+
+    if (req.headers.authorization) {
+      const token = getToken(req);
+      const decoded = jwt.verify(token, "meusecret");
+
+      currentUser = await User.findById(decoded.id);
+      currentUser.password = undefined;
+    } else {
+      currentUser = null;
+    }
+
+    res.status(200).send(currentUser);
   }
 };

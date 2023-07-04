5463fcf558a5383a582ff4990438e1f14dee808e
doguebart <douglasweelber.2004@outlook.com>
2023-07-04 18:03:26 -0300

    creating verify token function to create private routes

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -165,4 +165,10 @@ module.exports = class UserController {
       res.status(200).json({ user });
     }
   }
+
+  static async editUser(req, res) {
+    res.status(200).json({
+      message: "Deu certo o update",
+    });
+  }
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 
 const UserController = require("../controllers/UserController");
 
+// Middlewares
+const checkToken = require("../helpers/verify-token");
+
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
 router.get("/:id", UserController.getUserById);
+router.patch("/edit/:id", checkToken, UserController.editUser);
 
 module.exports = router;

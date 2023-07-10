eaa3ce631f48bc3529352fd51c28422473d95f95
doguebart <douglasweelber.2004@outlook.com>
2023-07-10 14:57:51 -0300

    creating useAuth hook

---
+++ frontend/get-a-pet/src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+// Api
+import api from "../utils/api";
+
+const useAuth = () => {
+  const register = async (user) => {
+    try {
+      const data = await api.post("/users/register", user).then((response) => {
+        return response.data;
+      });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { register };
+};
+
+export default useAuth;

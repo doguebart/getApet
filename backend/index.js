409029b69892b9fae70c6ef31a939b6168c9b050
doguebart <douglasweelber.2004@outlook.com>
2023-07-10 15:58:10 -0300

    changing cors configuration

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Solve cors
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 // Public folder for images
 app.use(express.static("public"));
